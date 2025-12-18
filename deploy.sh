bd3091d4a3465462c7bc30dd2dc4a9c0069c84f8
mac <mac@MacBook-Pro-mac.local>
2025-12-18 11:45:46 +0200

    Update deploy script with force reset

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # Force update files from Git
+# This script should be run by Hostinger on deployment
+cd "$(git rev-parse --show-toplevel)" || exit 1
 git fetch origin
 git reset --hard origin/main
 git clean -fd
+# Force update file timestamps
+touch assets/css/main.css assets/js/main.js header.php
 
