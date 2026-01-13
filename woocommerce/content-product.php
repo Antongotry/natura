527165ff622a3a1ff6878337438ebb0d3aa9cd2e
mac <mac@MacBook-Pro-mac.local>
2026-01-13 17:10:28 +0200

    Додано бейдж 'Акція' вверху справа на фото та стилі для цін зі знижкою
    - Бейдж тепер відображається вверху справа на фото товара (десктоп та мобільна)
    - Додано стилі для нової ціни зі знижкою (жирна, темна)
    - Додано стилі для старої ціни (закреслена, сіра)
    - Бейдж має зелений фон #4DBD56

--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -25,17 +25,16 @@ $product_card_title = get_post_meta( $product->get_id(), '_product_card_title', 
 ?>
 <li <?php wc_product_class( '', $product ); ?>>
 	<div class="product-card">
-		<div class="product-card__image-wrapper">
-			<a href="<?php echo esc_url( get_permalink() ); ?>" class="woocommerce-LoopProduct-link woocommerce-loop-product__link">
-				<?php echo $product->get_image( 'medium' ); ?>
-			</a>
-		</div>
-
+	<div class="product-card__image-wrapper">
+		<a href="<?php echo esc_url( get_permalink() ); ?>" class="woocommerce-LoopProduct-link woocommerce-loop-product__link">
+			<?php echo $product->get_image( 'medium' ); ?>
+		</a>
 		<?php if ( $product->is_on_sale() ) : ?>
 			<div class="product-card__badge-wrapper">
 				<?php woocommerce_show_product_loop_sale_flash(); ?>
 			</div>
 		<?php endif; ?>
+	</div>
 
 		<div class="product-card__content">
 			<div class="product-card__title-price-wrapper">
@@ -75,18 +74,18 @@ $product_card_title = get_post_meta( $product->get_id(), '_product_card_title', 
 					<?php woocommerce_template_loop_price(); ?>
 				</div>
 
-				<div class="product-card__button-wrapper">
-					<?php woocommerce_template_loop_add_to_cart(); ?>
-					<?php
-					// Выводим поле количества под кнопкой
-					wc_get_template(
-						'loop/quantity-input.php',
-						array(
-							'product'     => $product,
-							'input_value' => 1,
-						)
-					);
-					?>
+			<div class="product-card__button-wrapper">
+				<?php woocommerce_template_loop_add_to_cart(); ?>
+				<?php
+				// Выводим поле количества под кнопкой
+				wc_get_template(
+					'loop/quantity-input.php',
+					array(
+						'product'     => $product,
+						'input_value' => 1,
+					)
+				);
+				?>
 				</div>
 			</div>
 		</div>
