19e10012a39e295504073b612e750273b10d746d
mac <mac@MacBook-Pro-mac.local>
2026-01-22 14:26:32 +0200

    Trusted carousel: add static image (ordinary + hover)

--- template-parts/home/trusted.php
+++ template-parts/home/trusted.php
@@ -42,11 +42,28 @@ if (! defined('ABSPATH')) {
 			$bottom_items = ! empty($carousel_items['bottom']) ? $carousel_items['bottom'] : array();
 
 			// Top carousel (right)
-			if (! empty($top_items)) :
+			$trusted_static_icon = 'https://bisque-parrot-207888.hostingersite.com/wp-content/uploads/2026/01/Mask-group-1_result.webp';
+			$trusted_static_icon_hover = 'https://bisque-parrot-207888.hostingersite.com/wp-content/uploads/2026/01/Mask-group_result.webp';
+			if (! empty($top_items) || $trusted_static_icon || $trusted_static_icon_hover) :
 				?>
 				<div class="trusted__carousel trusted__carousel--right">
 					<div class="trusted__carousel-track">
 						<?php
+						// Static trusted image (ordinary + hover)
+						if ($trusted_static_icon || $trusted_static_icon_hover) {
+							?>
+							<div class="trusted__carousel-item">
+								<div class="trusted__item-content">
+									<?php if ($trusted_static_icon) : ?>
+										<img class="trusted__icon" src="<?php echo esc_url($trusted_static_icon); ?>" alt="">
+									<?php endif; ?>
+									<?php if ($trusted_static_icon_hover) : ?>
+										<img class="trusted__icon-hover" src="<?php echo esc_url($trusted_static_icon_hover); ?>" alt="">
+									<?php endif; ?>
+								</div>
+							</div>
+							<?php
+						}
 						// Render original items
 						foreach ($top_items as $item) {
 							$icon_id = isset($item['icon_id']) ? (int) $item['icon_id'] : 0;
@@ -70,6 +87,20 @@ if (! defined('ABSPATH')) {
 							}
 						}
 						// Duplicate items for seamless loop
+						if ($trusted_static_icon || $trusted_static_icon_hover) {
+							?>
+							<div class="trusted__carousel-item">
+								<div class="trusted__item-content">
+									<?php if ($trusted_static_icon) : ?>
+										<img class="trusted__icon" src="<?php echo esc_url($trusted_static_icon); ?>" alt="">
+									<?php endif; ?>
+									<?php if ($trusted_static_icon_hover) : ?>
+										<img class="trusted__icon-hover" src="<?php echo esc_url($trusted_static_icon_hover); ?>" alt="">
+									<?php endif; ?>
+								</div>
+							</div>
+							<?php
+						}
 						foreach ($top_items as $item) {
 							$icon_id = isset($item['icon_id']) ? (int) $item['icon_id'] : 0;
 							$icon_hover_id = isset($item['icon_hover_id']) ? (int) $item['icon_hover_id'] : 0;
